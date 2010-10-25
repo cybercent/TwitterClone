64567043484b713f2cb3164080be2850631052d8
Takeshi Sato <master.extelligence@gmail.com>
2010-10-25 18:53:11 +0900

    Story ID: 5855798 新規プロジェクトを作成。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterClone::Application.config.secret_token = '5894be25a3700fdc46ae15e920fce422043b0841074e0704ac0e72869cacca12c7887d54f59cc724436f18deb6d9c5e003eae94082c4362c3b8a884dac71d24e'
