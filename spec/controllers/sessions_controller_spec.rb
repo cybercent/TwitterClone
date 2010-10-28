640a61c23393904f0fe8c8c52487ef0f298f8f1b
y-hirasawa <y-hirasawa@netyear.net>
2010-10-28 13:18:52 +0900

    アカウント作成周りのモデル、コントローラテストを追加

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,29 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe "GET 'new'" do
-    it "should be successful" do
+  describe "GET 'Sign inページを閲覧するとき'" do
+    render_views
+
+    it "正常に表示されること" do
       get 'new'
       response.should be_success
     end
-  end
 
+    it "正しいタイトルが表示されること" do
+      get 'new'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Sign in")
+    end
+  end
+  
+  describe "POST 'Sign inページにて Sign inするとき'" do
+    describe "失敗するパターン" do
+      before(:each) do
+        @attr = { :email => "email@example.com", :password => "invalid" }
+      end
+      it "同一ページが表示されること" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,6 +14,11 @@ describe UsersController do
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Sign up")
     end
 
+    it "名前フィールドが表示されること" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
     it "正しくユーザー情報が保存されること" do
       #TODO:テストコードを書く。
     end
@@ -33,6 +38,74 @@ describe UsersController do
       get 'show', :id => @user
       assigns(:user).should == @user
     end
+
+    it "h1タグにユーザーの名前が表示されること" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "プロフィール画像が表示されること" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
   end
 
+  describe "POST '新規ユーザー作成するとき'" do
+    
+    describe "失敗するパターン" do
+      
+      before(:each) do
+        @attr = {
+          :name => "",
+          :email => "",
+          :password => "",
+          :password_confirmation => ""
+        }
+      end
+      
+      it "ユーザー新規登録ができないこと" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "正しいタイトルが表示されること" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+      
+      it "新規登録フォームが表示されること" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "成功するパターン" do
+      
+      before(:each) do
+        @attr = {
+          :name => "New User",
+          :email => "user@example.com",
+          :password => "foobar",
+          :password_confirmation => "foobar"
+        }
+      end
+      
+      it "ユーザーの新規登録ができること" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "つぶやきページが表示されること" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "ウェルカムメッセージが表示されること" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
+  end
 end
