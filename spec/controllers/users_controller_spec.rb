e9672ac5dd0e339d56740616f943a313622639bc
y-hirasawa <y-hirasawa@netyear.net>
2010-10-29 10:34:52 +0900

    ユーザー情報の削除機能を追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,6 +17,9 @@ describe UsersController do
         second = Factory(:user, :email => "another@example.com")
         third = Factory(:user, :email => "another@example.org")
         @users = [@user, second, third]
+        30.times do
+          @users << Factory(:user, :email => Factory.next(:email))
+        end
       end
       it "should be successful" do
         get :index
@@ -31,6 +34,19 @@ describe UsersController do
         @users.each do |u|
           response.should have_selector("li", :content => u.name)
         end
+      end
+      it "should have an element for each user" do
+        get :index
+        @users[0..2].each do |user|
+          response.should have_selector("li", :content => user.name)
+        end
+      end
+      it "should paginate users" do
+        get :index
+        response.should have_selector("div.pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/users?page=2", :content => "2")
+        response.should have_selector("a", :href => "/users?page=2", :content => "Next")
       end
     end
   end
@@ -267,6 +283,45 @@ describe UsersController do
       it "メールアドレスが不正の場合、ユーザー情報の更新ができないこと" do
         get :update, :id => @user, :user => {}
         response.should redirect_to(root_path)
+      end
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    describe "as a non-signed-in user" do
+      it "should deny access" do
+        delete :destroy, :id => @user
+        response.should redirect_to(signin_path)
+      end
+    end
+    
+    describe "as a non-admin user" do
+      it "should protect the page" do
+        test_sign_in(@user)
+        delete :destroy, :id => @user
+        response.should redirect_to(root_path)
+      end
+    end
+    
+    describe "as an admin user" do
+      before(:each) do
+        admin = Factory(:user, :email => "admin@example.com", :admin => true)
+        test_sign_in(admin)
+      end
+      
+      it "should destroy the user" do
+        lambda do
+          delete :destroy, :id => @user
+        end.should change(User, :count).by(-1)
+      end
+      
+      it "should redirect to the users page" do
+        delete :destroy, :id => @user
+        response.should redirect_to(users_path)
       end
     end
   end
