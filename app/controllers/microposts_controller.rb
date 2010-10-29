46e849b7a0cd5b1ae9d07ab0e9ca74de545a08e7
Takeshi Sato <master.extelligence@gmail.com>
2010-10-29 20:03:28 +0900

    Story ID: 5856091 つぶやきを削除できるようにした。

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -13,6 +14,14 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_path, :flash => { :success => "Micropost deleted!" }
   end
 
+  private
+
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micropost.user)
+  end
 end
